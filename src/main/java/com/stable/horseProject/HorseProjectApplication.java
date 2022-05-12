03aa37cde6a7f5da1eccf65c8241d527fb2f0311
김재현 <jaehyun@gimjaehyeon-ui-MacBookPro.local>
2022-05-12 15:48:31 +0900

    Initial commit

---
+++ src/main/java/com/stable/horseProject/HorseProjectApplication.java
@@ -0,0 +1,13 @@
+package com.stable.horseProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorseProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorseProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stable/horseProject/HorseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stable.horseProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorseProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
